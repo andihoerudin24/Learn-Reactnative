6ef35d44e521b5ac5a3181c6e3287177196a66b6
andihoerudin24 <andihoerudin24@gmail.com>
2020-03-07 12:05:49 +0700

    learning step 2

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Header from "./components/Header";
+import StartGameScreen from "./screens/StartGamesScreen";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screnn}>
+      <Header title="Guest a Number" />
+      <StartGameScreen />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  screnn: {
+    flex: 1
+  }
 });
